f0f20f14941cfb3df117966eebaccc35f28d256f
Geky <gekymedia@gmail.com>
2026-02-24 23:41:04 +0000

    Deploy: 2026-02-24 23:41:04

--- routes/web.php
+++ routes/web.php
@@ -420,6 +420,7 @@ Route::post('/settings/security', [SecurityController::class, 'update'])->name('
     |----------
     */
 Route::prefix('g')->name('groups.')->group(function () {
+    Route::get('/', [GroupController::class, 'index'])->name('index');
     Route::get('/{group}/messages/partial', [GroupController::class, 'messagesPartial'])
         ->name('messages.partial')
         ->middleware('auth');
